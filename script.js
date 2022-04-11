8d180ca68efbe55470668772af580aa34bbcbc03
Sthwalo <sthwaloe@gmail.com>
2022-04-11 16:16:59 +0200

    Start

---
+++ script.js
@@ -0,0 +1,48 @@
+
+var $window   = $(window),
+height    = $window.height(),
+width     = $window.width(),
+navheight = $('#nav_wrap').height();
+
+function sticky(){
+var scrollTop = $window.scrollTop();
+if (scrollTop > (height - navheight)) {
+$('#nav_wrap').addClass('sticky');
+$('nav').addClass('nav_animate');
+setTimeout(function(){
+  $('#logo').css({
+    'left': 3 + '%', 
+    'transition':'.5s'
+  });     
+  $('#social').css({
+    'right': 5 + '%', 
+    'transition':'.5s'
+  });
+}, 200);
+
+} else {
+$('#nav_wrap').removeClass('sticky');
+$('nav').removeClass('nav_animate');
+setTimeout(function(){
+  $('#logo').css({
+    'left':-150, 
+    'transition':'.5s'
+  });     
+  $('#social').css({
+    'right':-150, 
+    'transition':'.5s'
+  });
+}, 200);
+}
+} 
+
+$window.on('scroll', sticky);  
+
+//Navigational Menu
+$('nav a').click(function(a){
+var menuPlace = $(this).index();
+a.preventDefault();
+$('html, body').animate({
+scrollTop : $('section').eq(menuPlace).offset().top - $('nav').height()
+}, 700);
+});
